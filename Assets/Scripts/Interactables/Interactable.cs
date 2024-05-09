01c58b181a68c5d0213d875bf28d5fe35f63625d
ReaperAnon <reaperanon415@gmail.com>
2024-05-09 21:15:49 +0200

    added fading functionality to interactable UI canvas

--- Assets/Scripts/Interactables/Interactable.cs
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -8,27 +8,52 @@ namespace Twinfiltration
     [RequireComponent(typeof(SphereCollider))]
     public class Interactable : MonoBehaviour
     {
+        private const float m_FadeSpeed = 10;
+
         [SerializeField] private Transform m_InteractAnchor;
         [SerializeField] private GameObject m_InteractUI;
 
         private SphereCollider m_TriggerZone;
+        private CanvasGroup m_CanvasGroup;
+        private float m_AlphaTarget = 0f;
 
         private void Awake()
         {
             m_TriggerZone = GetComponent<SphereCollider>();
+            m_CanvasGroup = m_InteractUI.GetComponent<CanvasGroup>();
             m_InteractUI.SetActive(false);
+            m_CanvasGroup.alpha = 0f;
+        }
+
+        private void Update()
+        {
+            if (!m_InteractUI.activeInHierarchy)
+                return;
+
+            if (m_CanvasGroup.alpha == m_AlphaTarget)
+            {
+                if (m_AlphaTarget <= 0f)
+                    m_InteractUI.SetActive(false);
+
+                return;
+            }
+
+            var deltaTime = Time.deltaTime;
+            var fadeVal = m_CanvasGroup.alpha < m_AlphaTarget ? deltaTime : -deltaTime;
+            m_CanvasGroup.alpha += fadeVal * m_FadeSpeed;
         }
 
         private void OnTriggerEnter(Collider other)
         {
             Debug.Log("Entered interaction zone.");
             m_InteractUI.SetActive(true);
+            m_AlphaTarget = 1f;
         }
 
         private void OnTriggerExit(Collider other)
         {
             Debug.Log("Exited interaction zone");
-            m_InteractUI.SetActive(false);
+            m_AlphaTarget = 0f;
         }
     }
 }
