9cea222248dbae25d377dacfeb86844de972a698
CapitolineTriad <169301867+CapitolineTriad@users.noreply.github.com>
2024-05-09 17:15:13 +0200

    made enemy script, working multiplayer movement

---
+++ Assets/Scripts/Controllers/Player/EnemyController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Twinfiltration
+{
+    public class EnemyController : CharacterController
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+
+        protected override void GetMovementInput()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
