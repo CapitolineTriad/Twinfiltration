3565d81fd5ca08da018728a88bbaf0292711ff19
ReaperAnon <reaperanon415@gmail.com>
2024-05-09 21:39:17 +0200

    player movement input is rotated relative to camera yaw

--- Assets/Scripts/Controllers/Player/PlayerController.cs
+++ Assets/Scripts/Controllers/Player/PlayerController.cs
@@ -7,17 +7,21 @@ namespace Twinfiltration
     [RequireComponent(typeof(CapsuleCollider), typeof(Rigidbody))]
     public class PlayerController : CharacterController
     {
+        private Transform m_Camera;
+
         protected override void Awake()
         {
+            m_Camera = Camera.main.transform;
             base.Awake();
         }
 
         protected override void GetMovementInput()
         {
+            var viewYaw = Quaternion.Euler(0, m_Camera.rotation.eulerAngles.y, 0);
             float x = Input.GetAxisRaw("Horizontal");
             float z = Input.GetAxisRaw("Vertical");
 
-            m_TargetDir = new Vector3(x, 0, z);
+            m_TargetDir = viewYaw * new Vector3(x, 0, z);
             if (m_TargetDir.magnitude > 1)
                 m_TargetDir.Normalize();
         }
